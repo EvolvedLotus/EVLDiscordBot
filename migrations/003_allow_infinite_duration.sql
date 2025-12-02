b9ae1c2de382fc810c30fd8f8cf37f8e74066ae4
EVLTrendy <management@evolvedlotus.com>
2025-12-01 22:26:52 -0500

    Delete 003_allow_infinite_duration.sql

--- migrations/003_allow_infinite_duration.sql
+++
@@ -1,9 +0,0 @@
--- Migration to allow -1 for infinite duration in tasks
--- This removes the existing check constraint and adds a new one that allows -1
-
--- Drop the existing constraint
-ALTER TABLE tasks DROP CONSTRAINT IF EXISTS tasks_duration_hours_check;
-
--- Add new constraint that allows positive numbers OR -1 for infinite duration
-ALTER TABLE tasks ADD CONSTRAINT tasks_duration_hours_check 
-  CHECK (duration_hours > 0 OR duration_hours = -1);
